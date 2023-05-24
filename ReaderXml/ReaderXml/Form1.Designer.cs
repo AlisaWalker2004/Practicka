fa7d1ffa62ddb0e795cc78d7ef98ac6b0c355597
AlisaWalker2004 <116071232+AlisaWalker2004@users.noreply.github.com>
2023-05-24 20:31:35 +0400

    Чтение XML
    Приложение предназначено для чтения XML файлов любой сложности

---
+++ ReaderXml/ReaderXml/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ReaderXml
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listTables = new System.Windows.Forms.ListBox();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(9, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(246, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбрать файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listTables
+            // 
+            this.listTables.FormattingEnabled = true;
+            this.listTables.Location = new System.Drawing.Point(13, 127);
+            this.listTables.Name = "listTables";
+            this.listTables.Size = new System.Drawing.Size(506, 316);
+            this.listTables.TabIndex = 2;
+            this.listTables.SelectedIndexChanged += new System.EventHandler(this.listTables_SelectedIndexChanged);
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(548, 127);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(458, 316);
+            this.dataGrid.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 450);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.listTables);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listTables;
+        private System.Windows.Forms.DataGridView dataGrid;
+    }
+}
+
---
+++ ReaderXml/ReaderXml/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ReaderXml
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter =
+                "XML - files (*.xml)|*.xml|All files (*.*)|*.*";
+            openFileDialog1.RestoreDirectory = true;
+            dataSet = new DataSet();
+        }
+
+        private DataSet dataSet;
+
+        private void UpdateTableList()
+        {
+            listTables.Items.Clear();
+            foreach (DataTable table in dataSet.Tables) 
+            {
+                listTables.Items.Add(table.TableName);
+            }
+
+            if(listTables.Items.Count>0)
+                listTables.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataSet.Tables.Clear();
+            listTables.Items.Clear();
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    dataSet.ReadXml(openFileDialog1.FileName);
+
+                    dataSet.AcceptChanges();
+                    this.UpdateTableList();
+
+
+                    //string filname = openFileDialog1.FileName;
+                    //DataSet ds = new DataSet("table");
+
+                    //ds.ReadXml(filname);
+                    //var xd = XDocument.Load(filname);
+
+                    //foreach (var x in xd.Descendants())
+                    //foreach (var x in ds.Tables)
+                    //for (var x = 0; x <= ds.Tables.Count; x++)
+                    //{
+                    //    //if (x.HasElements)
+                    //    //{
+                    //    dataGridView1.DataSource = ds.Tables[x];
+                    //    //listBox1.Items.Add("\n" + x.Name + "\n");
+                    //    //}
+                    //    //else
+                    //    //{
+                    //    //    dataGridView1.DataSource = x;
+                    //    //    //listBox1.Items.Add("\n" + x.Name + "\t" + x.Value + "\n");
+                    //    //}
+                    //}
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listTables_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGrid.DataSource = dataSet;
+            dataGrid.DataMember = listTables.SelectedItem.ToString();
+        }
+    }
+}
