b3e82e29a169b860e417b26c5cf7433b430545aa
AlisaWalker2004 <116071232+AlisaWalker2004@users.noreply.github.com>
2023-06-02 23:19:00 +0400

    Приложение для хранения и записи номеров телефонов
    Завершение приложения

--- ContactNotebook/ContactNotebook/ContactNotebook/MainWindow.xaml.cs
+++ ContactNotebook/ContactNotebook/ContactNotebook/MainWindow.xaml.cs
@@ -22,16 +22,16 @@ namespace ContactNotebook
     /// </summary>
     public partial class MainWindow : Window
     {
-       
+
         public ClassForList list { get; set; } = new ClassForList();
 
         public MainWindow()
         {
             InitializeComponent();
 
-            list.fileRead = File.ReadAllLines("Контакты.txt", Encoding.UTF8).ToList();
+            var rows = File.ReadAllLines("Контакты.txt", Encoding.UTF8).OrderBy(x=>x.Split('\t')[3]);
+            list.fileRead = rows.ToList();
             DataContext = list;
-            
         }
 
         private void BtnEnter_Click(object sender, RoutedEventArgs e)
@@ -42,29 +42,57 @@ namespace ContactNotebook
 
         private void BtnSave_Click(object sender, RoutedEventArgs e)
         {
-            using (StreamWriter sw = new StreamWriter("Контакты.txt", true))
+            if (BtnEdit.Visibility == Visibility.Hidden)
             {
-                sw.Write("\nКабинет:\t" + tbKab.Text); sw.Write("\tДолжность или ФИО:\t" + tbFio.Text);
-                sw.Write("\tНомер телефона:\t" + tbNumberPhone.Text);
-                sw.Close();
+                list.fileRead.RemoveAt(lbReadText.SelectedIndex);
+                list.fileRead.Add("Кабинет:\t" + tbKab.Text + "\tДолжность или ФИО:\t" + tbFio.Text + "\tНомер телефона:\t" + tbNumberPhone.Text);
 
-                MessageBox.Show("Информация сохранена!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                TextWriter writer = new StreamWriter("Контакты.txt");
+                foreach (var item in lbReadText.Items)
+                    writer.WriteLine(item.ToString());
+                writer.Close();
 
                 tbKab.Text = ""; tbFio.Text = ""; tbNumberPhone.Text = "";
 
+                list.fileRead.Clear();
                 list.fileRead = File.ReadAllLines("Контакты.txt", Encoding.UTF8).ToList();
                 DataContext = list;
+                lbReadText.ItemsSource = list.fileRead;
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter("Контакты.txt", true))
+                {
+                    sw.Write("Кабинет:\t" + tbKab.Text); sw.Write("\tДолжность или ФИО:\t" + tbFio.Text);
+                    sw.Write("\tНомер телефона:\t" + tbNumberPhone.Text + "\n");
+                    sw.Close();
+
+                    MessageBox.Show("Информация сохранена!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                    tbKab.Text = ""; tbFio.Text = ""; tbNumberPhone.Text = "";
+
+                    list.fileRead.Clear();
+                }
+                list.fileRead = File.ReadAllLines("Контакты.txt", Encoding.UTF8).ToList();
+                DataContext = list;
+                lbReadText.ItemsSource = list.fileRead;
             }
         }
 
         private void BtnBack_Click(object sender, RoutedEventArgs e)
         {
+            var rows = File.ReadAllLines("Контакты.txt", Encoding.UTF8).OrderBy(x => x.Split('\t')[3]);
+            list.fileRead = rows.ToList();
+            DataContext = list;
+            lbReadText.ItemsSource = list.fileRead;
+
             GridVis.Visibility = Visibility.Hidden;
             BtnEdit.Visibility = Visibility.Hidden;
             BtnEnter.Visibility = Visibility.Visible;
             spAuto.Visibility = Visibility.Hidden;
             BtnBack.Visibility = Visibility.Hidden;
             BtnDelete.Visibility = Visibility.Hidden;
+
         }
 
         private void BtnVhod_Click(object sender, RoutedEventArgs e)
@@ -87,14 +115,8 @@ namespace ContactNotebook
 
         private void lbReadText_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (BtnEnter.Visibility == Visibility.Hidden)
-            {
-                
-                tbKab.Text = lbReadText.SelectedItem.ToString().Split('\t')[1];
-                tbFio.Text = lbReadText.SelectedItem.ToString().Split('\t')[3];
-                tbNumberPhone.Text = lbReadText.SelectedItem.ToString().Split('\t')[5];
-
-            }
+            if(BtnEnter.Visibility == Visibility.Hidden)
+                BtnEdit.Visibility = Visibility.Visible;
         }
 
         private void BtnDelete_Click(object sender, RoutedEventArgs e)
@@ -105,18 +127,31 @@ namespace ContactNotebook
             foreach (var item in lbReadText.Items)
                 writer.WriteLine(item.ToString());
             writer.Close();
+
+            tbKab.Text = ""; tbFio.Text = ""; tbNumberPhone.Text = "";
+
+            list.fileRead.Clear();
+            list.fileRead = File.ReadAllLines("Контакты.txt", Encoding.UTF8).ToList();
+            DataContext = list;
+            lbReadText.ItemsSource = list.fileRead;
         }
 
         private void BtnEdit_Click(object sender, RoutedEventArgs e)
         {
-            list.fileRead.RemoveAt(lbReadText.SelectedIndex);
-            list.fileRead.Add("Кабинет:\t" + tbKab.Text + "\tДолжность или ФИО:\t" + tbFio.Text + "\tНомер телефона:\t" + tbNumberPhone.Text);
-
-            TextWriter writer = new StreamWriter("Контакты.txt");
-            foreach (var item in lbReadText.Items)
-                writer.WriteLine(item.ToString());
-            writer.Close();
+            if (lbReadText.SelectedItem != null && BtnEdit.Content.ToString() == "Редактировать")
+            {
+                tbKab.Text = lbReadText.SelectedItem.ToString().Split('\t')[1];
+                tbFio.Text = lbReadText.SelectedItem.ToString().Split('\t')[3];
+                tbNumberPhone.Text = lbReadText.SelectedItem.ToString().Split('\t')[5];
+                BtnEdit.Visibility = Visibility.Hidden;
+            }
+            else
+                MessageBox.Show("Необходимо выбрать строку для изменения!", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
         }
 
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            tbKab.Text = ""; tbFio.Text = ""; tbNumberPhone.Text = "";
+        }
     }
 }
